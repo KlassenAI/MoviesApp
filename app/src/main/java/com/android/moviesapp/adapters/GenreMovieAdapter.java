19c6166789c622c926b7a41a29d08a0bef1fa596
Александр Классен <aiklassen@edu.hse.ru>
2020-11-02 11:41:04 +0500

    Save changes

--- app/src/main/java/com/android/moviesapp/adapters/GenreMovieAdapter.java
+++ app/src/main/java/com/android/moviesapp/adapters/GenreMovieAdapter.java
@@ -35,18 +35,44 @@ public class GenreMovieAdapter extends RecyclerView.Adapter<GenreMovieAdapter.Ge
 
     @Override
     public GenreMovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.genre_movie,
+        View view = LayoutInflater.from(mContext).inflate(R.layout.genre_item,
                 parent, false);
         return new GenreMovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(GenreMovieViewHolder holder, int position) {
+        String genre = getGenre(mGenres.get(position));
 
     }
 
     @Override
     public int getItemCount() {
         return mGenres.size();
+    }
+
+    public String getGenre(String id) {
+        switch (id) {
+            case "12": return "Adventure";
+            case "14": return "Fantasy";
+            case "16": return "Animation";
+            case "18": return "Drama";
+            case "27": return "Horror";
+            case "28": return "Action";
+            case "35": return "Comedy";
+            case "36": return "History";
+            case "37": return "Western";
+            case "53": return "Thriller";
+            case "80": return "Crime";
+            case "99": return "Documentary";
+            case "878": return "Science Fiction";
+            case "9648": return "Mystery";
+            case "10402": return "Music";
+            case "10749": return "Romance";
+            case "10751": return "Family";
+            case "10752": return "War";
+            case "10770": return "TV Movie";
+            default: return "No name";
+        }
     }
 }
